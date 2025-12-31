1c3f9c3aa32f1ef448c9ded0297d25730c1ab3f7
Subodh Shah <shahsubodh166@gmail.com>
2025-12-31 16:19:10 +0545

    modified hashmap file

--- hashmap.js
+++ hashmap.js
@@ -1,5 +1,3 @@
-// Module containing Hash map class
-
 class HashMap {
 	constructor(loadFactor = 0.75, capacity = 16) {
 		this.loadFactor = loadFactor;
@@ -10,8 +8,8 @@ class HashMap {
 	hash(key){
 		let hashCode = 0;
 		const primeNumber = 31;
-		for (let i = 0; i < k.length(); i++){
-			hashCode = (hashCode*primeNumber + key.charCodeAt(i)) % this.size();
+		for (let i = 0; i < key.length(); i++){
+			hashCode = (hashCode*primeNumber + key.charCodeAt(i)) % this.capacity;
 		}
 		return hashCode;
 	}
@@ -19,19 +17,32 @@ class HashMap {
 		
 	set(key, value) {
 		let hashCode = this.hash(key);
-		if(!this.buckets[hashCode]){}
+		if(!this.buckets[hashCode]){
+			this.buckets[hashCode] = [];
+		}
+		
+		let entry = this.buckets[hashCode].find(item => item[0] === key);
+		if(entry) {
+			entry[1] = value;
+		} else {
+			this.buckets[hashCode] = [key, value];
+		}
 	}
 	
 	get(key) {
 		let hashCode = this.hash(key);
-		return this.buckets[hashCode];
+		let entry =  this.buckets[hashCode].find(item => item[0] === key)
+		if(entry) {
+			return this.buckets[hashCode][1];
+		}
+		return undefined;
 	}
 	
 	remove(key) {
-		let hashCode = hash(key);
-		if(this.buckets.includes(key)) 
+		let hashCode = this.hash(key);
+		if(this.buckets[hashCode].includes([key, value])) 
 		{
-			this.buckets.remove(hashCode);
+			this.buckets.splice(hashCode, 1);
 			return true;
 		}
 		return false;
@@ -47,37 +58,42 @@ class HashMap {
 	
 	keys(){
 		let keysArray = [];
-		for (let key in this.hashMap) {
-			keysArray.push(key);
+		for (let entry in this.buckets) {
+			keysArray.push(entry[0]);
 		}
 		return keysArray;
 	}
 	
 	values() {
 		let valuesArray = [];
-		for (let key in this.hashMap) {
-			valuesArray.push(this.hashMap[keys]);
+		for (let entry in this.buckets) {
+			valuesArray.push(entry[1]);
 		}
 		return valuesArray;
 	}
 	
 	entries () {
 		let entriesArray = [];
-		for (let key in this.hashMap) {
-			entriesArray.push([key, this.hashMap[key]]);
+		for (let entry in this.buckets) {
+			entriesArray.push([entry[0], entry[1]]);
 		}
 		return entriesArray;
 	}
 	
 	size () {
 		const counter = 0;
-		for (let key in this.hashMap) {
+		for (let entry in this.buckets) {
 			counter++;
 		}
 		return counter;
 	}
 	
-	clear() {}
+	clear() {
+		for (let entry in this.buckets) {
+			let hashCode = this.hash(entry[0]);
+			this.buckets[hashCode] = [];
+		}
+	}
 }
 
 function mainFunction()  {
